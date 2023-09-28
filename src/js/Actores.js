6a0f03c9696fc6cf4fa1ceff528f5f63d0a3f906
Otavio Bandeira <otaviobandeira14@gmail.com>
2023-09-28 14:06:54 -0300

    criação do arquivo Actores utilizando poo.

---
+++ src/js/Actores.js
@@ -0,0 +1,33 @@
+export class Actores {
+  constructor(imagem, name, description){
+    this.imagem = imagem
+    this.name = name
+    this.description = description
+  }
+}
+
+const elizabethOlsenImg = `/src/assets/imagensAtores/Elizabeth Olsen.png`
+const xochitlGomezImg = `/src/assets/imagensAtores/Xochitl Gomez.png`
+const benedictCumberbatchImg = `/src/assets/imagensAtores/Benedict Cumberbatch.png`
+const benedictWongImg = `/src/assets/imagensAtores/Benedict Wong.png`
+const charlizeTheronImg = `/src/assets/imagensAtores/Charlize Theron.png`
+const hayleyAtwellImg = `/src/assets/imagensAtores/Hayley Atwell.png`
+const lashanaLynchImg = `/src/assets/imagensAtores/Lashana Lynch.png`
+const patrickStewartImg = `/src/assets/imagensAtores/Patrick Stewart.png`
+const rachelMcAdamsImg = `/src/assets/imagensAtores/Rachel McAdams.png`
+const johnKrasinskiImg = `/src/assets/imagensAtores/John Krasinski.png`
+const ansonMountImg = `/src/assets/imagensAtores/Anson Mount.png`
+const chiwetelEjioforImg = `/src/assets/imagensAtores/Chiwetel Ejiofor.png`
+
+export const xochitlGomez = new Actores(xochitlGomezImg, "Xochitl Gomez")
+export const benedictCumberbatch = new Actores(benedictCumberbatchImg, "Benedict Cumberbatch")
+export const elizabethOlsen = new Actores(elizabethOlsenImg, "Elizabeth Olsen", "...")
+export const benedictWong = new Actores(benedictWongImg, "Benedict Wong")
+export const charlizeTheron = new Actores(charlizeTheronImg, "Charlize Theron")
+export const hayleyAtwell = new Actores(hayleyAtwellImg, "Heyley Atwell")
+export const lashanaLynch = new Actores(lashanaLynchImg, "Lashana Lynch")
+export const patrickStewart = new Actores(patrickStewartImg, "Patrick Stewart")
+export const rachelMcAdams = new Actores(rachelMcAdamsImg, "Rachel McAdams")
+export const johnKrasinski = new Actores(johnKrasinskiImg, "John Krasinski")
+export const ansonMount = new Actores(ansonMountImg, "Anson Mount")
+export const chiwetelEjiofor = new Actores(chiwetelEjioforImg, "Chiwetel Ejiofor")

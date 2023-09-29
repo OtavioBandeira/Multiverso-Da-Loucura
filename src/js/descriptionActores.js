ba317b1d3a9d99fedfc3e767153e0c6c56de46af
Otavio Bandeira <otaviobandeira14@gmail.com>
2023-09-29 08:16:42 -0300

    inclusão do arquivo descriptionActores.js

---
+++ src/js/descriptionActores.js
@@ -0,0 +1,146 @@
+const descriptionActores = [
+  {
+    benedictCumberbatchDescription: `Benedict Cumberbatch é um renomado ator britânico conhecido por sua extraordinária versatilidade e habilidade de atuação excepcional. Ele nasceu em 19 de julho de 1976, em Londres, Inglaterra, e desde então, construiu uma carreira brilhante na indústria do entretenimento. Cumberbatch é frequentemente elogiado por sua presença magnética no palco e na tela, bem como por sua capacidade de se transformar em uma variedade de papéis desafiadores.
+
+    Sua notoriedade cresceu consideravelmente quando ele assumiu o papel principal na série de televisão "Sherlock" (2010-2017), onde interpretou o icônico detetive Sherlock Holmes, ganhando aclamação da crítica e um grande número de fãs em todo o mundo. Além disso, ele se destacou no cinema, interpretando papéis notáveis, incluindo o Doutor Estranho no Universo Cinematográfico Marvel, o matemático Alan Turing em "O Jogo da Imitação" (2014) e muitos outros.
+    
+    Cumberbatch é admirado por sua capacidade de dar vida a personagens complexos e intrigantes, além de sua voz característica que o tornou uma escolha popular para dublagens e narrações. Sua carreira continua a crescer, e ele é considerado um dos atores mais respeitados e talentosos de sua geração.`
+  },
+  {
+    elizabethOlsenDescription: `Elizabeth Olsen é uma talentosa atriz americana que ganhou reconhecimento e admiração por suas atuações versáteis e cativantes. Ela nasceu em 16 de fevereiro de 1989 em Sherman Oaks, Califórnia, nos Estados Unidos, e faz parte de uma família com raízes na indústria do entretenimento, com as irmãs gêmeas Mary-Kate e Ashley Olsen.
+
+    Elizabeth Olsen se destacou especialmente por seu papel como Wanda Maximoff, também conhecida como Feiticeira Escarlate, no Universo Cinematográfico Marvel. Sua interpretação do personagem em séries como "WandaVision" (2021) e nos filmes da franquia, como "Vingadores: A Era de Ultron" (2015), conquistou tanto crítica quanto público, solidificando seu lugar como uma das estrelas em ascensão de Hollywood.
+    
+    Além de seu trabalho no MCU, Elizabeth Olsen também participou de filmes aclamados, como "Martha Marcy May Marlene" (2011) e "Vingadores: Ultimato" (2019), demonstrando sua versatilidade como atriz.
+    
+    Sua presença carismática na tela, habilidade de interpretar personagens complexos e dedicação à sua arte a tornam uma figura admirada na indústria cinematográfica e uma atriz em constante ascensão.`
+  },
+  {
+    benedictWongDescription: `Benedict Wong é um talentoso ator britânico de origem chinesa que se destacou na indústria do entretenimento por suas notáveis atuações e versatilidade. Ele nasceu em 3 de junho de 1971, em Manchester, Inglaterra. Wong é conhecido por sua presença magnética e sua capacidade de interpretar uma variedade de personagens de forma convincente.
+
+    Uma das interpretações mais notáveis de Benedict Wong foi o papel de Wong, o Mestre das Artes Místicas, no Universo Cinematográfico Marvel. Ele apareceu como Wong em filmes como "Doutor Estranho" (2016), "Vingadores: Guerra Infinita" (2018) e "Vingadores: Ultimato" (2019), cativando o público com sua atuação carismática.
+    
+    Além de seu trabalho na Marvel, Benedict Wong tem uma carreira diversificada no cinema e na televisão britânicos, com papéis em produções notáveis, como "O Senhor dos Anéis: As Duas Torres" (2002) e "Interestelar" (2014). Sua versatilidade como ator e presença marcante na tela o tornaram uma figura respeitada na indústria do entretenimento.`
+  },
+  {
+    xochitlGomezDescription: `Xochitl Gomez é uma talentosa atriz mexicana-americana que ficou conhecida por sua atuação marcante em produções de destaque. Ela nasceu em 30 de março de 2006, tornando-se uma das jovens promessas da indústria do entretenimento.
+
+    Gomez ganhou destaque ao interpretar o papel de America Chavez na série da Marvel Studios, "Doutor Estranho no Multiverso da Loucura" (2022). Sua interpretação cativante de uma personagem superpoderosa e sua representação como a primeira super-heroína LGBTQ+ do Universo Cinematográfico Marvel foram elogiadas tanto pela crítica quanto pelos fãs.
+    
+    Além de seu trabalho na Marvel, Xochitl Gomez também participou de outros projetos, demonstrando sua versatilidade como atriz e seu potencial para uma carreira promissora no cinema e na televisão. Sua presença na indústria do entretenimento tem sido marcada por seu talento, carisma e contribuição para a diversidade e inclusão na mídia.`
+  },
+  {
+    ansonMountDescription: `Anson Mount é um ator norte-americano conhecido por sua presença carismática e versatilidade em uma variedade de papéis em filmes e séries de televisão. Ele nasceu em 25 de fevereiro de 1973, em Mount Prospect, Illinois, EUA.
+
+    Mount ganhou destaque ao longo de sua carreira com uma série de atuações notáveis. Uma das interpretações mais conhecidas de Mount foi como Cullen Bohannon na série de faroeste "Hell on Wheels" (2011-2016), na qual ele desempenhou o papel principal e conquistou elogios da crítica e do público por sua atuação convincente como um ex-soldado confederado em busca de vingança após a Guerra Civil Americana.
+    
+    Além disso, ele também participou de outros projetos notáveis, incluindo papéis em filmes como "O Grande Assalto" (2001) e "Cabeça a Prêmio" (2007) e aparições em séries populares, como "Star Trek: Discovery," onde interpretou o Capitão Christopher Pike.
+    
+    A habilidade de Anson Mount em interpretar personagens diversos e complexos o tornou um ator respeitado na indústria do entretenimento, e sua carreira continua a evoluir com novos projetos e desafios.`
+  },
+  {
+    rachelMcAdamsDescription: `Rachel McAdams é uma renomada atriz canadense conhecida por sua versatilidade e talento em uma variedade de gêneros cinematográficos. Ela nasceu em 17 de novembro de 1978, em London, Ontário, Canadá, e ao longo de sua carreira, conquistou reconhecimento internacional por suas atuações excepcionais.
+
+    McAdams ganhou destaque com papéis em filmes populares, como:
+    
+    1. "Meninas Malvadas" (2004): Neste filme cult, ela interpretou a icônica personagem Regina George, tornando-se uma figura querida pelos fãs.
+    
+    2. "Diário de uma Paixão" (2004): Ela protagonizou este romance emocional, baseado no livro de Nicholas Sparks, ao lado de Ryan Gosling, cativando o público com sua química na tela.
+    
+    3. "Sherlock Holmes" (2009) e sua sequência "Sherlock Holmes: O Jogo de Sombras" (2011): Ela desempenhou o papel de Irene Adler, contracenando com Robert Downey Jr. e Jude Law.
+    
+    4. "Meia-Noite em Paris" (2011): McAdams trabalhou com o diretor Woody Allen neste filme, que foi aclamado pela crítica.
+    
+    5. "Spotlight - Segredos Revelados" (2015): Ela recebeu elogios da crítica e uma indicação ao Oscar por sua atuação neste drama baseado em eventos reais sobre a investigação jornalística de casos de abuso sexual na Igreja Católica.
+    
+    Rachel McAdams é conhecida por sua capacidade de dar vida a uma variedade de personagens complexos e por sua presença cativante na tela. Sua carreira continua a prosperar, e ela permanece como uma das atrizes mais respeitadas e amadas da indústria cinematográfica.`
+  },
+  {
+    patrickStewartDescription: `Sir Patrick Stewart é um renomado ator britânico conhecido por sua carreira versátil e sua atuação icônica em diversos palcos e telas. Ele nasceu em 13 de julho de 1940, em Mirfield, West Yorkshire, Inglaterra, e ao longo de sua longa carreira, conquistou reconhecimento global e uma base de fãs dedicada.
+
+    Patrick Stewart é mais conhecido por sua interpretação do Capitão Jean-Luc Picard na série de televisão "Star Trek: A Nova Geração" (1987-1994), bem como em vários filmes da franquia "Star Trek." Seu papel como o líder intelectual e carismático da nave estelar Enterprise o tornou uma figura icônica na cultura pop e um embaixador da série "Star Trek."
+    
+    Além de seu trabalho em "Star Trek," Stewart também teve uma carreira teatral de sucesso, atuando em diversas peças, incluindo performances aclamadas em produções de Shakespeare. Ele é conhecido por sua habilidade em dar vida a personagens complexos e multifacetados tanto no palco quanto na tela.
+    
+    Ao longo de sua carreira, Patrick Stewart recebeu inúmeros prêmios e honrarias, incluindo o título de cavaleiro da Ordem do Império Britânico (Knight Bachelor) em 2010 por seus serviços à atuação dramática. Sua dedicação à arte da atuação e sua influência duradoura na indústria do entretenimento o tornam uma figura respeitada e amada por fãs e colegas de profissão em todo o mundo.`
+  },
+  {
+    lashanaLynchDescription: `Lashana Lynch é uma talentosa atriz britânica que ganhou destaque na indústria do entretenimento por suas atuações notáveis em filmes e séries de televisão. Ela nasceu em 27 de novembro de 1987, em Hammersmith, Londres, Reino Unido, e ao longo de sua carreira, tem conquistado reconhecimento por sua versatilidade e presença cativante na tela.
+
+    Uma das interpretações mais notáveis de Lashana Lynch ocorreu quando ela foi escalada como a agente Nomi no filme "007: Sem Tempo Para Morrer" (2021), da franquia James Bond. Sua atuação como a primeira agente 00 mulher da série foi elogiada por trazer uma nova perspectiva à icônica franquia de espionagem.
+    
+    Além disso, Lashana Lynch também se destacou em outros projetos, incluindo seu papel em "Capitã Marvel" (2019), onde interpretou Maria Rambeau, amiga da personagem-título. Sua atuação carismática e talento a tornaram uma atriz em ascensão na indústria do entretenimento, e os fãs aguardam com expectativa suas futuras interpretações e projetos. Ela também é reconhecida por sua representação positiva e inclusiva no cinema, promovendo a diversidade na mídia.`
+  },
+  {
+    johnKrasinskiDescription: `John Krasinski é um versátil ator, diretor, roteirista e produtor americano conhecido por sua carreira em filmes e séries de televisão. Ele nasceu em 20 de outubro de 1979, em Newton, Massachusetts, EUA, e ganhou destaque por seu carisma e talento na indústria do entretenimento.
+
+    Krasinski é mais conhecido por seu papel como Jim Halpert na série de comédia de sucesso "The Office" (US) (2005-2013), onde interpretou um funcionário de escritório que se tornou um dos personagens mais adorados da televisão.
+    
+    Além de sua atuação, John Krasinski também é conhecido por seus esforços como diretor e roteirista. Ele dirigiu e estrelou o filme de suspense "Um Lugar Silencioso" (2018), que recebeu elogios da crítica e se tornou um sucesso de bilheteria, gerando uma sequência, "Um Lugar Silencioso - Parte II" (2021). Sua habilidade em criar tensão e contar histórias emocionais conquistou tanto o público quanto a indústria cinematográfica.
+    
+    Krasinski também é reconhecido por seu compromisso com projetos de entretenimento de qualidade e pela diversidade de papéis que desempenha. Sua versatilidade como ator e seu sucesso em várias áreas do entretenimento o tornam uma figura respeitada na indústria e um talento admirado pelos fãs.`
+  },
+  {
+    hayleyAtwellDescription: `Hayley Atwell é uma talentosa atriz britânica conhecida por sua versatilidade e talento em filmes, séries de televisão e teatro. Ela nasceu em 5 de abril de 1982, em Londres, Inglaterra, e ao longo de sua carreira, conquistou reconhecimento internacional por suas atuações notáveis.
+
+    Atwell ganhou destaque com seu papel como Peggy Carter no Universo Cinematográfico Marvel, aparecendo pela primeira vez como a agente da SSR em "Capitão América: O Primeiro Vingador" (2011) e continuando a interpretar a personagem em várias outras produções, incluindo a série de televisão "Agente Carter" (2015-2016). Sua interpretação como Peggy Carter conquistou fãs e críticos, tornando-a uma figura icônica no mundo dos super-heróis.
+    
+    Além de seu trabalho na Marvel, Hayley Atwell também é conhecida por papéis em filmes e séries notáveis, como "A Duquesa" (2008), "Cinderela" (2015) e a série "Conviction" (2016).
+    
+    Ela é admirada por sua capacidade de dar vida a personagens complexos e pela presença carismática que ela traz para suas performances. Atwell continua a ser uma atriz respeitada na indústria do entretenimento e uma presença querida pelos fãs. Além de seu trabalho como atriz, ela também está envolvida em causas sociais e filantrópicas.`
+  },
+  {
+    chiwetelEjioforDescription: `Chiwetel Ejiofor é um talentoso ator britânico de origem nigeriana que se destacou na indústria do entretenimento por suas atuações impressionantes em uma variedade de papéis no cinema, televisão e teatro. Ele nasceu em 10 de julho de 1977, em Forest Gate, Londres, Inglaterra, e ao longo de sua carreira, conquistou reconhecimento internacional por seu talento.
+
+    Ejiofor ganhou destaque com sua atuação no filme "12 Anos de Escravidão" (2013), dirigido por Steve McQueen. Sua interpretação do protagonista Solomon Northup, um homem livre que é sequestrado e vendido como escravo, lhe rendeu aclamação da crítica e uma indicação ao Oscar de Melhor Ator, entre muitos outros prêmios. O filme ganhou o Oscar de Melhor Filme, solidificando o status de Ejiofor como um ator de classe mundial.
+    
+    Além de "12 Anos de Escravidão," Chiwetel Ejiofor participou de uma série de outros filmes notáveis, incluindo "Crianças Invisíveis" (2005), "Doutor Estranho" (2016), onde interpretou o vilão Mordo, e "The Martian" (2015).
+    
+    Ejiofor é elogiado por sua capacidade de se transformar em personagens complexos e sua presença cativante na tela. Ele também é conhecido por seu trabalho no teatro e por sua dedicação à arte da atuação. Sua carreira continua a prosperar, e ele é considerado um dos atores mais talentosos e respeitados de sua geração.`
+  },
+  {
+    charlizeTheronDescription: `Charlize Theron é uma talentosa atriz, produtora e modelo sul-africana-americana que se destacou na indústria do entretenimento por sua versatilidade, beleza e talento excepcionais. Ela nasceu em 7 de agosto de 1975 em Benoni, África do Sul, e ao longo de sua carreira, conquistou reconhecimento internacional e diversos prêmios por suas atuações notáveis.
+
+    Theron ganhou destaque em Hollywood com papéis em filmes como:
+    
+    1. "Monster - Desejo Assassino" (2003): Neste filme, ela interpretou a assassina em série Aileen Wuornos, uma atuação que lhe rendeu o Oscar de Melhor Atriz e consagrou sua reputação como uma das atrizes mais talentosas de sua geração.
+    
+    2. "Mad Max: Estrada da Fúria" (2015): Theron brilhou como Furiosa, uma personagem forte e destemida, neste filme de ação pós-apocalíptico dirigido por George Miller.
+    
+    3. "Atômica" (2017): Ela desempenhou o papel de uma agente secreta em um filme de espionagem cheio de ação, demonstrando sua versatilidade em diferentes gêneros.
+    
+    Além de sua atuação, Charlize Theron também é uma produtora bem-sucedida, envolvida em projetos que abordam questões sociais e políticas importantes. Ela é conhecida por sua beleza deslumbrante e por sua habilidade em se transformar para se adequar a uma variedade de papéis no cinema.
+    
+    Sua carreira é marcada por prêmios e honrarias, e ela continua a ser uma das atrizes mais respeitadas e influentes da indústria cinematográfica, tanto por suas performances quanto por seu compromisso com a arte e a justiça social.`
+  },
+]
+
+export const descriptions = descriptionActores.map(descriptionActores => {
+  return {
+    benedictCumberbatchDescription: descriptionActores.benedictCumberbatchDescription,
+    elizabethOlsenDescription: descriptionActores.elizabethOlsenDescription,
+    benedictWongDescription: descriptionActores.benedictWongDescription,
+    xochitlGomezDescription: descriptionActores.xochitlGomezDescription,
+    ansonMountDescription: descriptionActores.ansonMountDescription,
+    rachelMcAdamsDescription: descriptionActores.rachelMcAdamsDescription,
+    patrickStewartDescription: descriptionActores.patrickStewartDescription,
+    lashanaLynchDescription: descriptionActores.lashanaLynchDescription,
+    johnKrasinskiDescription: descriptionActores.johnKrasinskiDescription,
+    hayleyAtwellDescription: descriptionActores.hayleyAtwellDescription,
+    chiwetelEjioforDescription: descriptionActores.chiwetelEjioforDescription,
+    charlizeTheronDescription: descriptionActores.charlizeTheronDescription,
+  }
+})
+
+export const benedictCumberbatchDescription = descriptions[0].benedictCumberbatchDescription
+export const elizabethOlsenDescription = descriptions[1].elizabethOlsenDescription
+export const benedictWongDescription = descriptions[2].benedictWongDescription
+export const xochitlGomezDescription = descriptions[3].xochitlGomezDescription
+export const ansonMountDescription = descriptions[4].ansonMountDescription
+export const rachelMcAdamsDescription = descriptions[5].rachelMcAdamsDescription
+export const patrickStewartDescription = descriptions[6].patrickStewartDescription
+export const lashanaLynchDescription = descriptions[7].lashanaLynchDescription
+export const johnKrasinskiDescription = descriptions[8].johnKrasinskiDescription
+export const hayleyAtwellDescription = descriptions[9].hayleyAtwellDescription
+export const chiwetelEjioforDescription = descriptions[10].chiwetelEjioforDescription
+export const charlizeTheronDescription = descriptions[11].charlizeTheronDescription

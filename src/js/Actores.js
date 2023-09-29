3d3f626b05cb9666bf3d193bba726c26d824bf96
Otavio Bandeira <otaviobandeira14@gmail.com>
2023-09-29 08:14:31 -0300

    importação do arquivo descriptionActores e as descrição dos personagens.

--- src/js/Actores.js
+++ src/js/Actores.js
@@ -1,3 +1,5 @@
+import { ansonMountDescription, benedictCumberbatchDescription, benedictWongDescription, charlizeTheronDescription, chiwetelEjioforDescription, elizabethOlsenDescription, hayleyAtwellDescription, johnKrasinskiDescription, lashanaLynchDescription, patrickStewartDescription, rachelMcAdamsDescription, xochitlGomezDescription } from "./descriptionActores.js"
+
 export class Actores {
   constructor(imagem, name, description){
     this.imagem = imagem
@@ -19,15 +21,15 @@ const johnKrasinskiImg = `/src/assets/imagensAtores/John Krasinski.png`
 const ansonMountImg = `/src/assets/imagensAtores/Anson Mount.png`
 const chiwetelEjioforImg = `/src/assets/imagensAtores/Chiwetel Ejiofor.png`
 
-export const xochitlGomez = new Actores(xochitlGomezImg, "Xochitl Gomez")
-export const benedictCumberbatch = new Actores(benedictCumberbatchImg, "Benedict Cumberbatch")
-export const elizabethOlsen = new Actores(elizabethOlsenImg, "Elizabeth Olsen", "...")
-export const benedictWong = new Actores(benedictWongImg, "Benedict Wong")
-export const charlizeTheron = new Actores(charlizeTheronImg, "Charlize Theron")
-export const hayleyAtwell = new Actores(hayleyAtwellImg, "Heyley Atwell")
-export const lashanaLynch = new Actores(lashanaLynchImg, "Lashana Lynch")
-export const patrickStewart = new Actores(patrickStewartImg, "Patrick Stewart")
-export const rachelMcAdams = new Actores(rachelMcAdamsImg, "Rachel McAdams")
-export const johnKrasinski = new Actores(johnKrasinskiImg, "John Krasinski")
-export const ansonMount = new Actores(ansonMountImg, "Anson Mount")
-export const chiwetelEjiofor = new Actores(chiwetelEjioforImg, "Chiwetel Ejiofor")
+export const xochitlGomez = new Actores(xochitlGomezImg, "Xochitl Gomez", xochitlGomezDescription)
+export const benedictCumberbatch = new Actores(benedictCumberbatchImg, "Benedict Cumberbatch", benedictCumberbatchDescription)
+export const elizabethOlsen = new Actores(elizabethOlsenImg, "Elizabeth Olsen", elizabethOlsenDescription)
+export const benedictWong = new Actores(benedictWongImg, "Benedict Wong", benedictWongDescription)
+export const charlizeTheron = new Actores(charlizeTheronImg, "Charlize Theron", charlizeTheronDescription)
+export const hayleyAtwell = new Actores(hayleyAtwellImg, "Heyley Atwell", hayleyAtwellDescription)
+export const lashanaLynch = new Actores(lashanaLynchImg, "Lashana Lynch", lashanaLynchDescription)
+export const patrickStewart = new Actores(patrickStewartImg, "Patrick Stewart", patrickStewartDescription)
+export const rachelMcAdams = new Actores(rachelMcAdamsImg, "Rachel McAdams", rachelMcAdamsDescription)
+export const johnKrasinski = new Actores(johnKrasinskiImg, "John Krasinski", johnKrasinskiDescription)
+export const ansonMount = new Actores(ansonMountImg, "Anson Mount", ansonMountDescription)
+export const chiwetelEjiofor = new Actores(chiwetelEjioforImg, "Chiwetel Ejiofor", chiwetelEjioforDescription)

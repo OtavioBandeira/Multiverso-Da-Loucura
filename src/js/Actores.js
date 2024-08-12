c165fbb96eff4aab0a5c60380fd70bb893640b30
Otavio Bandeira <otaviobandeira14@gmail.com>
2024-08-12 19:32:21 -0300

    chore: removed file actores,descriptionActores,charactersSelected,theme and app

--- src/js/Actores.js
+++
@@ -1,7 +0,0 @@
-export class Actores {
-  constructor(imagem, name, description){
-    this.imagem = imagem
-    this.name = name
-    this.description = description
-  }
-}
---
+++ src/js/database.js
@@ -0,0 +1,110 @@
+export const database = [
+  {
+    characterBanner: "",
+    characterName: "",
+    characterDescription: "",
+    characterImages: [],
+    actorName: "",
+    actorDescription: "",
+    actorImages: []
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    characterDescription: "",
+    characterImages: [],
+    actorName: "",
+    actorDescription: "",
+    actorImages: []
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    characterDescription: "",
+    characterImages: [],
+    actorName: "",
+    actorDescription: "",
+    actorImages: []
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    characterDescription: "",
+    characterImages: [],
+    actorName: "",
+    actorDescription: "",
+    actorImages: []
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    characterDescription: "",
+    characterImages: [],
+    actorName: "",
+    actorDescription: "",
+    actorImages: []
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    characterDescription: "",
+    characterImages: [],
+    actorName: "",
+    actorDescription: "",
+    actorImages: []
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    characterDescription: "",
+    characterImages: [],
+    actorName: "",
+    actorDescription: "",
+    actorImages: []
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    characterDescription: "",
+    characterImages: [],
+    actorName: "",
+    actorDescription: "",
+    actorImages: []
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    characterDescription: "",
+    characterImages: [],
+    actorName: "",
+    actorDescription: "",
+    actorImages: []
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    characterDescription: "",
+    characterImages: [],
+    actorName: "",
+    actorDescription: "",
+    actorImages: []
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    characterDescription: "",
+    characterImages: [],
+    actorName: "",
+    actorDescription: "",
+    actorImages: []
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    characterDescription: "",
+    characterImages: [],
+    actorName: "",
+    actorDescription: "",
+    actorImages: []
+  }
+]

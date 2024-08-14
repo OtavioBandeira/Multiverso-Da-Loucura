b277a2d3350aa6de0555de111bee8980bae6fd76
Otavio Bandeira <otaviobandeira14@gmail.com>
2024-08-14 13:20:28 -0300

    chore: created folder database

---
+++ src/database/database.js
@@ -0,0 +1,162 @@
+export const database = [
+  {
+    characterBanner: "",
+    characterName: "Feiticeira-Escarlate",
+
+    characterDescription: `A Feiticeira Escarlate, cujo nome verdadeiro é Wanda Maximoff, é uma personagem da Marvel Comics, conhecida por seu papel como uma das heroínas mais poderosas do universo Marvel. Ela é retratada como uma mulher de aparência marcante, com longos cabelos ruivos, olhos intensos e uma expressão que frequentemente reflete tanto poder quanto vulnerabilidade.
+
+    Wanda é geralmente vista usando um traje vermelho que varia de versões mais simples a mais complexas, dependendo da era ou do artista. Seu uniforme clássico consiste em um colante vermelho, com luvas longas e botas que combinam com o restante da vestimenta. Ela também usa uma tiara distinta em forma de "M", que se tornou um símbolo icônico da personagem. Este traje, combinado com uma capa fluida, lhe dá uma aparência majestosa e poderosa.
+
+    Como Feiticeira Escarlate, Wanda é conhecida por seus poderes imensos e muitas vezes imprevisíveis. Originalmente, seus poderes eram descritos como manipulação de probabilidades, mas ao longo dos anos, suas habilidades evoluíram para a manipulação da realidade em larga escala. Ela pode alterar o tecido da realidade, criando ou destruindo mundos, mudando eventos passados ou futuros, e influenciando a mente e as emoções das pessoas ao seu redor. Seus poderes são tanto uma bênção quanto uma maldição, pois ela muitas vezes luta para controlar suas habilidades e as consequências devastadoras que elas podem causar.
+
+    Em termos de personalidade, Wanda é complexa e multifacetada. Ela é profundamente leal às pessoas que ama, especialmente seu irmão gêmeo, Pietro Maximoff, também conhecido como Mercúrio. No entanto, sua vida é marcada por tragédias e perdas, o que a torna uma figura trágica no universo Marvel. Essa dor e sofrimento, combinados com seus imensos poderes, fazem dela uma personagem que oscila entre heroína e vilã, dependendo das circunstâncias.
+
+    Wanda também tem uma rica história de relacionamentos, sendo talvez o mais significativo seu romance e eventual casamento com o androide Visão. Esta relação, que desafia as normas e expectativas, destaca o desejo de Wanda por uma vida normal e por amor, mesmo sendo uma das pessoas mais poderosas do universo.
+
+    Seu desenvolvimento ao longo dos anos, tanto nos quadrinhos quanto nas adaptações para filmes e séries, reflete uma jornada de autodescoberta, luta interna e busca por equilíbrio. Ela é frequentemente retratada como alguém que luta para manter sua humanidade em meio a um poder que a isola dos outros e até mesmo de si mesma.
+
+    Wanda Maximoff, como Feiticeira Escarlate, é uma personagem que encapsula a tensão entre poder e responsabilidade, amor e perda, humanidade e divindade, tornando-a uma das figuras mais intrigantes e poderosas do universo Marvel.`,
+
+    actorName: "Elizabeth Olsen",
+    actorDescription: `Elizabeth Olsen é uma atriz americana, conhecida principalmente por seu papel como Wanda Maximoff, a Feiticeira Escarlate, no Universo Cinematográfico da Marvel (MCU). Nascida em 16 de fevereiro de 1989, em Sherman Oaks, Califórnia, Elizabeth é irmã mais nova das famosas gêmeas Mary-Kate e Ashley Olsen.
+
+    Ela é reconhecida por sua versatilidade e talento, transitando entre papéis em filmes independentes e grandes produções de Hollywood. Além de sua atuação em filmes de super-heróis, Olsen também ganhou elogios por suas performances em dramas como Martha Marcy May Marlene e Wind River.
+
+    Elizabeth Olsen é conhecida por sua beleza natural, com cabelos loiros que ela ocasionalmente tinge de castanho, e olhos verdes expressivos. Ela tem uma presença marcante na tela, combinando carisma com uma intensidade emocional que dá profundidade aos seus personagens. Além de seu talento como atriz, Olsen é admirada por sua humildade e profissionalismo dentro e fora das telas.`,
+
+    characterImages: [],
+    actorImages: [],
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    actorName: "",
+    
+    characterDescription: ``,
+
+    actorDescription: ``,
+    
+    characterImages: [],
+    actorImages: [],
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    actorName: "",
+    
+    characterDescription: ``,
+
+    actorDescription: ``,
+    
+    characterImages: [],
+    actorImages: [],
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    actorName: "",
+    
+    characterDescription: ``,
+
+    actorDescription: ``,
+    
+    characterImages: [],
+    actorImages: [],
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    actorName: "",
+    
+    characterDescription: ``,
+
+    actorDescription: ``,
+    
+    characterImages: [],
+    actorImages: [],
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    actorName: "",
+    
+    characterDescription: ``,
+
+    actorDescription: ``,
+    
+    characterImages: [],
+    actorImages: [],
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    actorName: "",
+    
+    characterDescription: ``,
+
+    actorDescription: ``,
+    
+    characterImages: [],
+    actorImages: [],
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    actorName: "",
+    
+    characterDescription: ``,
+
+    actorDescription: ``,
+    
+    characterImages: [],
+    actorImages: [],
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    actorName: "",
+    
+    characterDescription: ``,
+
+    actorDescription: ``,
+    
+    characterImages: [],
+    actorImages: [],
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    actorName: "",
+    
+    characterDescription: ``,
+
+    actorDescription: ``,
+    
+    characterImages: [],
+    actorImages: [],
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    actorName: "",
+    
+    characterDescription: ``,
+
+    actorDescription: ``,
+    
+    characterImages: [],
+    actorImages: [],
+  },
+  {
+    characterBanner: "",
+    characterName: "",
+    actorName: "",
+    
+    characterDescription: ``,
+
+    actorDescription: ``,
+    
+    characterImages: [],
+    actorImages: [],
+  },
+];

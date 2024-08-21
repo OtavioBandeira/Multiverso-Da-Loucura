34af418f2038d138573ac191d994e219d445377a
Otavio Bandeira <otaviobandeira14@gmail.com>
2024-08-21 19:15:31 -0300

    feat: creation of character cards

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,17 @@ import { database } from '../database/database.js'
 import { createCard } from './createCard.js'
 import { menuLinks,carousel } from './scripts.js'
 
+database.forEach(data => {
+  const { characterName,subtitles,characterBanner } = data
+
+  const characterData = {
+    characterName,
+    subtitles,
+    characterBanner
+  }
+
+  createCard(characterData)
+})
+
 menuLinks()
 carousel()
